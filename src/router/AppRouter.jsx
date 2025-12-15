20bc515236e2672b87ecb170b0a883474a1ef9c0
Sabbir Islam <sabbirislamalvi070800@gmail.com>
2025-12-16 00:54:10 +0600

    Add backend dependencies and update styles
    Added backend-related dependencies (express, cors, dotenv, mysql2) to package.json for future server/API integration. Updated global, app, and navbar CSS files for improved styling. Minor updates to AppRouter.jsx for routing adjustments.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,27 +1,29 @@
+// AppRouter.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import RecognizerPage from "../pages/RecognizerPage";
-import Navbar from "../components/Navbar";
 import UploadPage from "../pages/UploadPage";
 import HistoryPage from "../pages/HistoryPage";
 import DictionaryPage from "../pages/DictionaryPage";
+import Navbar from "../components/Navbar";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/recognizer" element={<RecognizerPage />} />
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/upload" element={<UploadPage />} />
-        <Route path="/history" element={<HistoryPage />} />
-        <Route path="/dictionary" element={<DictionaryPage />} />
-      </Routes>
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/recognizer" element={<RecognizerPage />} />
+          <Route path="/upload" element={<UploadPage />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/dictionary" element={<DictionaryPage />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 }
